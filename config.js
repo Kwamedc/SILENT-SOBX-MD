39b7e6e811f7d4df00314cf4efdcc9f5087640ec
Kwamedc <kwamedc87@gmail.com>
2024-11-08 15:35:14 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "37InlSSb#_rug94U1IPmWUjAze8jhDzSsOfYL_4f_djBhlKNMpRg",
 MONGODB: process.env.MONGODB || "enter mongodb url",
 };
